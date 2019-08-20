cab90fbb296be4117c41a1c6ca5ac50f27b42117
Wallyson Silva <wallysonferreirasilva@gmail.com>
2019-08-20 18:03:32 -0300

    Deleting Department view and controller

--- SistemaWebMvcVendedores/Startup.cs
+++ SistemaWebMvcVendedores/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SistemaWebMvcVendedores.Models;
 
 namespace SistemaWebMvcVendedores
 {
@@ -33,6 +35,9 @@ namespace SistemaWebMvcVendedores
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SistemaWebMvcVendedoresContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SistemaWebMvcVendedoresContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
