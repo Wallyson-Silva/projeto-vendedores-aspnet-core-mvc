98089cc85780c210cbd33fbe0fc072089df6e5ed
Wallyson Silva <wallysonferreirasilva@gmail.com>
2019-08-20 17:53:54 -0300

    First Model-Controller-View - Department

--- SistemaWebMvcVendedores/Controllers/HomeController.cs
+++ SistemaWebMvcVendedores/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SistemaWebMvcVendedores.Models;
+using SistemaWebMvcVendedores.Models.ViewModels;
 
 namespace SistemaWebMvcVendedores.Controllers
 {
--- SistemaWebMvcVendedores/Models/ViewModels/ErrorViewModel.cs
+++ SistemaWebMvcVendedores/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SistemaWebMvcVendedores.Models
+namespace SistemaWebMvcVendedores.Models.ViewModels
 {
     public class ErrorViewModel
     {
