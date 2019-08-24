205f742d4f5bf5a579a62e1621f6395e903a75fe
Wallyson Silva <wallysonferreirasilva@gmail.com>
2019-08-24 18:14:46 -0300

    Update Seller and Custom Service Exception

--- SistemaWebMvcVendedores/Controllers/SellersController.cs
+++ SistemaWebMvcVendedores/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SistemaWebMvcVendedores.Models;
 using SistemaWebMvcVendedores.Models.ViewModels;
 using SistemaWebMvcVendedores.Services;
+using SistemaWebMvcVendedores.Services.Exceptions;
 
 namespace SistemaWebMvcVendedores.Controllers
 {
@@ -44,13 +45,13 @@ namespace SistemaWebMvcVendedores.Controllers
 
         public IActionResult Delete(int? id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
             var obj = _sellerService.FindById(id.Value);
-            if(obj == null)
+            if (obj == null)
             {
                 return NotFound();
             }
@@ -80,8 +81,49 @@ namespace SistemaWebMvcVendedores.Controllers
             }
 
             return View(obj);
-
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
---
+++ SistemaWebMvcVendedores/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaWebMvcVendedores.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
